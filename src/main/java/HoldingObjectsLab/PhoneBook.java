b4fdf37ced50af9af2d69309bf9756b497052214
Zachary Wasserman <Zdwasserman@gmail.com>
2016-09-29 15:06:02 -0400

    part 3

--- src/main/java/HoldingObjectsLab/PhoneBook.java
+++ src/main/java/HoldingObjectsLab/PhoneBook.java
@@ -1,39 +1,43 @@
 package HoldingObjectsLab;
 import java.util.Map.*;
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ArrayList;
 /**
  * Created by zacharywasserman on 9/28/16.
  */
 public class PhoneBook {
     public Map<String, ArrayList> phoneBook = new HashMap();
-    public ArrayList<String> numbers = new ArrayList();
 
     public Object lookUp(String name){
         return phoneBook.get(name);
-        }
-    public void add(String name, String number){
-        numbers.add(number);
+    }
+    public void add(String name, ArrayList numbers){
         phoneBook.put(name, numbers);
     }
-    public void remove(String name){
+    public void removeEntry(String name){
         phoneBook.remove(name);
     }
     public void display(){
         System.out.println(phoneBook);
     }
-    public String reverseLookUp(String number){
-        for(Entry<String, ArrayList> entry : phoneBook.entrySet()){
-            if(numbers.contains(number)) {
-                return entry.getKey();
+    public String reverseLookUp(String number,ArrayList<String> numbers) {
+        for (Entry<String, ArrayList> entry : phoneBook.entrySet()) {
+            for (String num : numbers) {
+                if (num.equals(number)) {
+                    return entry.getKey();
+                }
+
             }
+
 
         }
         return "Number not found in book";
-
+    }
+    public void removeNum(String number, ArrayList<String> numbers){
+        numbers.remove(number);
     }
 
+}
 
-    }
 
--- src/test/java/Wasserman/Zachary/HoldingObjectsLabTest/PhoneBookTest.java
+++ src/test/java/Wasserman/Zachary/HoldingObjectsLabTest/PhoneBookTest.java
@@ -12,69 +12,88 @@ public class PhoneBookTest {
     public void lookUpTest(){
         PhoneBook book = new PhoneBook();
         HashMap phoneBook = new HashMap();
-        phoneBook.put("Zach", "111 111-1111");
+        ArrayList numbers = new ArrayList();
+        numbers.add("111 111-1111");
+        phoneBook.put("Zach",numbers);
         Object actual = book.lookUp("Zach");
-        Object expected = phoneBook.get(1);
+        Object expected = phoneBook.get(0);
         assertEquals(expected, actual);
     }
     @Test
     public void lookUpTestFailed(){
         PhoneBook book = new PhoneBook();
         HashMap phoneBook = new HashMap();
-        phoneBook.put("Zach", "111 111-1111");
+        ArrayList numbers = new ArrayList();
+        numbers.add("111 111-1111");
+        phoneBook.put("Zach", numbers);
         Object actual = book.lookUp("Fred");
         Object expected = null;
         assertEquals(expected, actual);
     }
     @Test
-    public void add() {
+    public void addTest() {
         PhoneBook book = new PhoneBook();
-        HashMap phoneBook = new HashMap();
         ArrayList numbers = new ArrayList();
-        book.add("Zach", "111 111-1111");
+        numbers.add("111 111-1111");
+        book.add("Zach", numbers);
         int actual = book.phoneBook.size();
         int expected = 1;
         assertEquals(expected, actual);
     }
     @Test
-    public void remove(){
+    public void removeEntryTest(){
         PhoneBook book = new PhoneBook();
-        book.add("Zach","111 111-1111");
-        book.add("bob", "222 222-2222");
-        book.remove("bob");
+        ArrayList numbers = new ArrayList();
+        book.add("Zach",numbers);
+        book.add("bob", numbers);
+        book.removeEntry("bob");
         int actual = book.phoneBook.size();
         int expected = 1;
         assertEquals(expected, actual);
     }
     @Test
-    public void removeFailed(){
+    public void removeFailedTest(){
         PhoneBook book = new PhoneBook();
-        book.add("Zach", "111 111-1111");
-        book.add("bob", "222 222-2222");
-        book.remove("fred");
+        ArrayList numbers = new ArrayList();
+        numbers.add("111 111-1111");
+        book.add("Zach",numbers);
+        book.removeEntry("fred");
         int actual = book.phoneBook.size();
-        int expected = 2;
+        int expected = 1;
         assertEquals(expected, actual);
     }
     @Test
     public void reverseLookUpTest(){
         PhoneBook book = new PhoneBook();
-        book.add("Zach", "111 111-1111");
-        book.add("bob", "222 222-2222");
-        book.remove("fred");
-        String actual = book.reverseLookUp("111 111-1111");
+        //HashMap phoneBook = new HashMap();
+        ArrayList numbers = new ArrayList();
+        //String num = "111 111-1111";
+        numbers.add("111 111-1111");
+        book.add("Zach",numbers);
+        String actual = book.reverseLookUp("111 111-1111", numbers);
         String expected = "Zach";
         assertEquals(expected, actual);
     }
     @Test
     public void reverseLookUpFailedTest(){
         PhoneBook book = new PhoneBook();
-
-        book.add("Zach", "111 111-1111");
-        book.add("bob", "222 222-2222");
-        book.remove("fred");
-        String actual = book.reverseLookUp("111 111-1112");
+        ArrayList numbers = new ArrayList();
+        numbers.add("111 111-1111");
+        book.add("Zach",numbers);
+        String actual = book.reverseLookUp("111 111-1112", numbers);
         String expected = "Number not found in book";
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void removeNumTest(){
+        PhoneBook book = new PhoneBook();
+        ArrayList<String> numbers = new ArrayList();
+        numbers.add("111 111-1111");
+        numbers.add("111 111-1112");
+        book.add("Zach",numbers);
+        book.removeNum("111 111-1112", numbers);
+        int actual = numbers.size();
+        int expected = 1;
         assertEquals(expected, actual);
     }
 }
